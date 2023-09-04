ab93029b9e6dc23d62cd084c83bd71806b0fab7d
Deepak996x <127407747+Deepak996x@users.noreply.github.com>
2023-09-04 20:31:20 +0530

    Add project files.

---
+++ AzureAppToCloud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureAppToCloud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
